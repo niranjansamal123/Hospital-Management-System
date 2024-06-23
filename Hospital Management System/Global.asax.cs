0726ca2fa36b34d6913606f3dcd2847189a86c0b
𝓜𝓲 𝓛𝓪𝓷 <niranjansamal1999@gmail.com>
2024-06-24 00:31:31 +0530

    Initial commit

---
+++ Hospital Management System/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Hospital_Management_System.Models;
+using Hospital_Management_System.Helpers; // Ensure this is correct
+
+namespace Hospital_Management_System
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize roles and admin user
+            RoleActions roleActions = new RoleActions();
+            roleActions.CreateAdmin();
+
+            DeleteAnnouncementOnExpire.DeleteAnnouncement();
+        }
+
+        protected void Application_BeginRequest()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+            Response.Cache.SetNoStore();
+        }
+    }
+}
---
+++ Hospital Management System/Helpers/RoleActions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Hospital_Management_System.Models;
+
+namespace Hospital_Management_System.Helpers
+{
+    public class RoleActions
+    {
+        public void CreateAdmin()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+                // Create Admin role if it doesn't exist
+                if (!roleManager.RoleExists("Admin"))
+                {
+                    var role = new IdentityRole { Name = "Admin" };
+                    roleManager.Create(role);
+                }
+
+                // Create Admin user if it doesn't exist
+                var user = userManager.FindByName("admin@admin.com");
+                if (user == null)
+                {
+                    user = new ApplicationUser { UserName = "admin@admin.com", Email = "admin@admin.com" };
+                    var adminUserResult = userManager.Create(user, "Password123!");
+
+                    // Assign user to Admin role
+                    if (adminUserResult.Succeeded)
+                    {
+                        userManager.AddToRole(user.Id, "Admin");
+                    }
+                }
+            }
+        }
+    }
+}
